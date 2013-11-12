b567ecbb4569f958bc9861f4bfcfbccaa6077200
sighforever <sighforever@gmail.com>
2013-11-12 22:35:22 +0800

    Change ExampleBean run

--- java/spring/springCore/src/main/java/example/ExampleBean.java
+++ java/spring/springCore/src/main/java/example/ExampleBean.java
@@ -2,13 +2,23 @@ package example;
 
 public class ExampleBean {
     private AnotherBean beanOne;
+    private YetAnotherBean beanTwo;
 
 
     public void run() {
+
+        this.beanOne.run();
+        this.beanTwo.run();
         System.out.println("ExampleBean");
     }
 
     public void SetBeanOne(AnotherBean bean) {
         this.beanOne = bean;
     }
+
+    public void SetBeanTwo(YetAnotherBean bean) {
+        this.beanTwo = bean; 
+    }
 }
+
+
