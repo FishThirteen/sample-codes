30cf829b0ea0dfe99490fdf4082accdb89d9c9db
sighforever <sighforever@gmail.com>
2014-01-28 19:02:33 +0800

    spring annotation

---
+++ java/spring/springCore/spring-core-annotation-cross/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import org.springframework.beans.annotations.Configuration;
+import org.springframework.beans.annotations.Bean;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
+
+
+@Configuration
+class ServiceConfig {
+  
+
+}
